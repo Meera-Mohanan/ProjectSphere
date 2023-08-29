b2ed66c7d17d3cbd3047b86dc77eb132be64b057
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-08-29 18:16:53 +0800

    AuthPage.js added with TODO LOGO

---
+++ client/src/pages/AuthPage.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import Login from '../components/Login'
+import SignUp from '../components/SignUp';
+// ToDo
+// import logo from '';
+
+function AuthPage() {
+  const [showSignUp, setShowSignUp] = useState(false);
+
+  const toggleSignUp = () => {
+    setShowSignUp((prevState) => !prevState);
+  };
+
+  return (
+    <div className='auth-page'>
+      <div className='logo-container'>
+      {/* <img className='logo-lg' src={logo} alt='ProjectSphere Logo'/> */}
+      </div>
+      <div className='form-container'>
+      {showSignUp ? <SignUp /> : <Login />}
+      <p className="sign-login-btn"onClick={toggleSignUp} style={{ cursor: 'pointer' }}>
+        {showSignUp ? 'Back to Login' : 'New here? Sign up'}
+      </p>
+      </div>
+    </div>
+  );
+}
+
+export default AuthPage;
