fc07c1785dc9caed10eaf44be27213a98d0d9a2d
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-08-29 18:57:28 +0800

    NavBar completed/logo pending

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import { useNavigate } from "react-router-dom";
+// import logo from '';
+import Auth from "../../utils/auth"
+
+function NavBar() {
+  function logout() {
+    // Clear user token and profile data from localStorage
+    localStorage.removeItem("id_token");
+    // this will reload the page and reset the state of the application
+    navigate("/");
+  }
+  const navigate = useNavigate();
+
+  return (
+    <div className="nav-bar">
+      {/* <img className='logo' src={logo} alt='ProjectSphere Logo'/> */}
+      <div className="links">
+        <Link to="/home">Home</Link>
+        <Link to="/" onClick={() => Auth.logout()}>
+          Logout
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
