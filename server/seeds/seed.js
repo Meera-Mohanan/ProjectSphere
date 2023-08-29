81161815848f1b0016dcd238abb95c135f6754c4
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-08-29 16:44:05 +0800

    seeders completed and working

---
+++ server/seeds/seed.js
@@ -0,0 +1,29 @@
+const db = require('../config/connection');
+const { User, Project, Task } = require('../models');
+const usersSeeds = require('./usersSeeds.json');
+const projectsSeeds = require('./projectSeeds.json');
+const tasksSeeds = require('./tasksSeeds.json');
+
+db.once('open', async () => {
+  try {
+    await Project.deleteMany({});
+    await User.deleteMany({});
+    await Task.deleteMany({});
+
+    // Seed Users
+    await User.create(usersSeeds);
+
+    // Seed Projects 
+    await Project.create(projectsSeeds);
+
+    // Seed Tasks
+    await Task.create(tasksSeeds);
+
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+
+  console.log('all done!');
+  process.exit(0);
+});
