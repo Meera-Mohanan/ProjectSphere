4f0a2d0c2ea0bd6e46bf91fe05dfac4865d9c3e7
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-08-29 18:24:16 +0800

    Home/Pages.js completed and folders added

---
+++ client/src/pages/Home.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useQuery } from '@apollo/client';
+
+
+import { QUERY_PROJECTS } from '../utils/queries';
+
+import ProjectList from '../components/Projectlist';
+import Sidebar from '../components/SideBar';
+import Nav from '../components/Nav';
+import Footer from "../components/Footer";
+
+const Home = () => {
+    const { loading, data } = useQuery(QUERY_PROJECTS);
+    const projects = data?.projects || [];
+
+    return (
+      <main>
+        <Nav/>
+        <div className='homeContent'>
+          <div className="sidebar">
+            <Sidebar projects={projects}/>
+          </div>
+          <div className="project-area my-3">
+            { loading ? 
+              (
+                <div>Loading...</div>
+              ) : ( 
+                <ProjectList projects={projects}/>
+              )
+            }
+          </div>
+        </div>
+        <div className="footerContent">
+          <Footer/>
+        </div>
+      </main>
+    );  
+};
+
+export default Home;
