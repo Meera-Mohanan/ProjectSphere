f62fd879968155251c18ea434db70e40815f4d65
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-08-29 20:12:43 +0800

    Project component completed

---
+++ client/src/components/Project/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Project = () => {
+  
+    return (
+        <div className="col-12 col-xl-6">
+            <div className="card mb-3">
+                <h4 className="card-header bg-dark text-light p-2 m-0">
+                    Project: NAME <br />
+                </h4>
+                <h4 className="card-header bg-dark text-light p-2 m-0">task: TASK LIST</h4>
+            </div>
+        </div>
+    );
+};
+
+export default Project;
