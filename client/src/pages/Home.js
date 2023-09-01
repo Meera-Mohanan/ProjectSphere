824d946f0f35fdb4736da0a59f5fabdcdcbef943
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-09-01 17:12:35 +0800

    pathway confirmed

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,40 +1,38 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { useState } from 'react';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { useState } from "react";
 
-import { QUERY_PROJECTS } from '../utils/queries';
+import { QUERY_PROJECTS } from "../utils/queries";
 
-import ProjectList from '../components/ProjectList';
-import Sidebar from '../components/SideBar';
-import Nav from '../components/Nav';
+import ProjectList from "../components/ProjectList";
+import Sidebar from "../components/SideBar";
+import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 
 const Home = () => {
-    const { loading, data } = useQuery(QUERY_PROJECTS);
-    const projects = data?.projects || [];
+  const { loading, data } = useQuery(QUERY_PROJECTS);
+  const projects = data?.projects || [];
 
-    return (
-      <main>
-        <Nav/>
-        <div className='homeContent'>
-          <div className="sidebar">
-            <Sidebar projects={projects}/>
-          </div>
-          <div className="project-area my-3">
-            { loading ? 
-              (
-                <div>Loading...</div>
-              ) : ( 
-                <ProjectList projects={projects}/>
-              )
-            }
-          </div>
+  return (
+    <main>
+      <Nav />
+      <div className="homeContent">
+        <div className="sidebar">
+          <Sidebar projects={projects} />
         </div>
-        <div className="footerContent">
-          <Footer/>
+        <div className="project-area my-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <ProjectList projects={projects} />
+          )}
         </div>
-      </main>
-    );  
+      </div>
+      <div className="footerContent">
+        <Footer />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
