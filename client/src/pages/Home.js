764b67226bb9493896e2f2ee05ea8c5d601c8f47
Meera-Mohanan <126405858+Meera-Mohanan@users.noreply.github.com>
2023-09-02 22:46:06 +0800

    removed project list component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,41 +1,48 @@
-import React from 'react';
-import { useQuery } from '@apollo/client';
-import { QUERY_PROJECTS } from '../utils/queries';
-import Nav from '../components/Nav';
-import ProjectList from '../components/ProjectList/index';
-import Sidebar from '../components/SideBar';
+import React from "react";
+import { useQuery } from "@apollo/client";
+import { QUERY_PROJECTS } from "../utils/queries";
+import Nav from "../components/Nav";
+
+// console.log(
+//   "Checking import path for ProjectList:",
+//   "../components/ProjectList"
+// );
+
+// import ProjectList from "/components/ProjectList";
+import Sidebar from "../components/SideBar";
 import Footer from "../components/Footer";
 
 // // Add a console.log statement to check the import path
-console.log("Checking import path for ProjectList:", '../components/ProjectList');
-
+console.log(
+  "Checking import path for ProjectList:",
+  "../components/ProjectList"
+);
 
 const Home = () => {
-    const { loading, data } = useQuery(QUERY_PROJECTS);
-    const projects = data?.projects || [];
+  const { loading, data } = useQuery(QUERY_PROJECTS);
+  const projects = data?.projects || [];
 
-    return (
-      <main>
-        <Nav/>
-        <div className='homeContent'>
-          <div className="sidebar">
-            <Sidebar projects={projects}/>
-          </div>
-          <div className="project-area my-3">
-            { loading ? 
-              (
-                <div>Loading...</div>
-              ) : ( 
-                <ProjectList projects={projects}/>
-              )
-            }
-          </div>
+  return (
+    <main>
+      <Nav />
+      <div className="homeContent">
+        <div className="sidebar">
+          <Sidebar projects={projects} />
         </div>
-        <div className="footerContent">
-          <Footer/>
+        <div className="project-area my-3">
+          {loading ? (
+            <div>Loading...</div>
+          ) : (
+            <h3>Project List Component</h3>
+            // <ProjectList projects={projects} />
+          )}
         </div>
-      </main>
-    );  
+      </div>
+      <div className="footerContent">
+        <Footer />
+      </div>
+    </main>
+  );
 };
 
 export default Home;
